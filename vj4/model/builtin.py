2db67c3923ab58958c9dac94514cff6945c2905d
Wende Tan <twd2@163.com>
2016-05-11 22:29:41 +0800

    links

--- vj4/model/builtin.py
+++ vj4/model/builtin.py
@@ -93,6 +93,6 @@ VIEW_LANGS = collections.OrderedDict([('zh_CN', '简体中文'),
                                       ('en', 'English')])
 
 # Footer extra HTMLs.
-FOOTER_EXTRA_HTMLS = ['© 2005 - 2016 Vijos.org', '{$GIT_REVISION}', 
+FOOTER_EXTRA_HTMLS = ['© 2005 - 2016 <a href="https://vijos.org/">Vijos.org</a>', '{$GIT_REVISION}', 
                       '<a href="http://www.miitbeian.gov.cn/" target="_blank" rel="nofollow">' +
                       '沪ICP备14040537号</a>']
