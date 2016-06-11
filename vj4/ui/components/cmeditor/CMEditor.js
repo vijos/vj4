e4bfa26d8d4babcc16b43c87ae5321cfba11315d
Josh Ouyang <JoshOY789@gmail.com>
2016-06-11 21:21:52 +0800

    Pack up markdown editor to component

---
+++ vj4/ui/components/cmeditor/CMEditor.js
@@ -0,0 +1,84 @@
+import SimpleMDE from 'simple-cmeditor';
+import objectAssign from 'object-assign';
+
+export default class CMEditor extends SimpleMDE {
+
+  constructor(options) {
+    const defaultOptions = {
+      toolbar: [
+        {
+          name: 'bold',
+          action: SimpleMDE.toggleBold,
+          iconClassName: 'icon-bold',
+          title: 'Bold',
+        },
+        {
+          name: 'italic',
+          action: SimpleMDE.toggleItalic,
+          iconClassName: 'icon-italic',
+          title: 'Italic',
+        },
+        '|',
+        {
+          name: 'quote',
+          action: SimpleMDE.toggleBlockquote,
+          iconClassName: 'icon-quote',
+          title: 'Quote',
+        },
+        {
+          name: 'unordered-list',
+          action: SimpleMDE.toggleUnorderedList,
+          iconClassName: 'icon-unordered_list',
+          title: 'Unordered List',
+        },
+        {
+          name: 'ordered-list',
+          action: SimpleMDE.toggleOrderedList,
+          iconClassName: 'icon-ordered_list',
+          title: 'Ordered List',
+        },
+        '|',
+        {
+          name: 'link',
+          action: SimpleMDE.drawLink,
+          iconClassName: 'icon-link',
+          title: 'Create Link',
+          default: true,
+        },
+        {
+          name: 'image',
+          action: SimpleMDE.drawImage,
+          iconClassName: 'icon-insert--image',
+          title: 'Insert Image',
+          default: true,
+        },
+        '|',
+        {
+          name: 'preview',
+          action: SimpleMDE.togglePreview,
+          iconClassName: 'icon-preview',
+          className: 'no-disable',
+          title: 'Toggle Preview',
+          default: true,
+        },
+        {
+          name: 'guide',
+          action: 'https://simplemde.com/markdown-guide',
+          iconClassName: 'icon-help',
+          className: 'no-disable',
+          title: 'Markdown Guide',
+          default: true,
+        },
+      ],
+      commonmark: {
+        safe: true,
+      },
+    };
+    if (options) {
+      super(objectAssign({}, defaultOptions, options));
+    }
+    else {
+      super(defaultOptions);
+    }
+  }
+}
--- vj4/ui/pages/discussion_create.page.js
+++ vj4/ui/pages/discussion_create.page.js
@@ -4,81 +4,12 @@ import 'simple-cmeditor/dist/simplemde.min.css';
 const page = new NamedPage('discussion_create', () => {
 
   require.ensure([
-    'simple-cmeditor/dist/simplemde.min.js',
+    'components/cmeditor/CMEditor',
   ], (require) => {
-    const SimpleMDE = require('simple-cmeditor/dist/simplemde.min.js');
-
-    new SimpleMDE({
+    const CMEditor = require('components/cmeditor/CMEditor').default;
+    new CMEditor({
       element: document.getElementById('markdown-textarea'),
-      toolbar: [
-        {
-          name: 'bold',
-          action: SimpleMDE.toggleBold,
-          iconClassName: 'icon-bold',
-          title: 'Bold',
-        },
-        {
-          name: "italic",
-          action: SimpleMDE.toggleItalic,
-          iconClassName: 'icon-italic',
-          title: 'Italic',
-        },
-        '|',
-        {
-          name: 'quote',
-          action: SimpleMDE.toggleBlockquote,
-          iconClassName: 'icon-quote',
-          title: 'Quote',
-        },
-        {
-          name: 'unordered-list',
-          action: SimpleMDE.toggleUnorderedList,
-          iconClassName: 'icon-unordered_list',
-          title: 'Unordered List',
-        },
-        {
-          name: 'ordered-list',
-          action: SimpleMDE.toggleOrderedList,
-          iconClassName: 'icon-ordered_list',
-          title: 'Ordered List',
-        },
-        '|',
-        {
-          name: 'link',
-          action: SimpleMDE.drawLink,
-          iconClassName: 'icon-link',
-          title: 'Create Link',
-          default: true,
-        },
-        {
-          name: 'image',
-          action: SimpleMDE.drawImage,
-          iconClassName: 'icon-insert--image',
-          title: 'Insert Image',
-          default: true,
-        },
-        '|',
-        {
-          name: 'preview',
-          action: SimpleMDE.togglePreview,
-          iconClassName: 'icon-preview',
-          className: 'no-disable',
-          title: 'Toggle Preview',
-          default: true,
-        },
-        {
-          'name': 'guide',
-          action: 'https://simplemde.com/markdown-guide',
-          iconClassName: "icon-help",
-          className: 'no-disable',
-          title: 'Markdown Guide',
-          default: true,
-        },
-      ],
-      spellChecker: false,
-      safe: true,
     });
-
   });
 
 });
