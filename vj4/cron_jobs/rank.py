b15c93c44b83f47d84cbc9420947f4ef950be9ed
blackbbc <505968815@qq.com>
2016-06-11 15:06:32 +0800

    fix dictionary unordered bug and use options

--- vj4/cron_jobs/rank.py
+++ vj4/cron_jobs/rank.py
@@ -1,34 +1,30 @@
 from pymongo import MongoClient
 from pymongo.errors import BulkWriteError
 
-DB_HOST = 'localhost'
-DB_PORT = 27017
-DB_NAME = 'test'
-COLL_NAME = 'user'
+from vj4.util import options
 
-CONN = MongoClient(DB_HOST, DB_PORT)
-DB = CONN[DB_NAME]
-COLL = DB[COLL_NAME]
+options.define('db_host', default='localhost', help='Database hostname or IP address.')
+options.define('db_port', default=27017, help='Database port.')
+options.define('db_name', default='test', help='Database name.')
+options.define('coll_name', default='user', help='User collection')
 
 # Key represents level
 # Value represents percent
-# E.g. "10": 1 means that people who rank in 1% will get 10 levels
-level_config = {
-    "10": 1,
-    "9": 2,
-    "8": 3,
-    "7": 5,
-    "6": 10,
-    "5": 20,
-    "4": 30,
-    "3": 50,
-    "2": 80,
-    "1": 100
-}
+# E.g. 10: 1 means that people who rank in 1% will get 10 levels
+level_config = [
+    (10, 1),
+    (9, 2),
+    (8, 3),
+    (7, 5),
+    (6, 10),
+    (5, 20),
+    (4, 30),
+    (3, 50),
+    (2, 80),
+    (1, 100)
+  ]
 
 def rank_data(udocs):
-  global level_config
-
   stack = list()
   result = list()
   rank = 1
@@ -55,23 +51,30 @@ def count_level(perc):
 
   perc *= 100
 
-  for key, value in level_config.items():
+  for item in level_config:
+    key = item[0]
+    value = item[1]
     if perc <= value:
-      return int(key)
+      return key
 
 def handle_rank():
-  udocs = list(COLL.find())
+  conn = MongoClient(options.options.db_host, options.options.db_port)
+  db = conn[options.options.db_name]
+  coll = db[options.options.coll_name]
+
+  udocs = list(coll.find())
   udocs.sort(key = lambda x:(x['rp']), reverse=True)
 
   rank_array = rank_data(udocs)
   total = rank_array[-1]
 
   index = 0
-  bulk = COLL.initialize_unordered_bulk_op()
+  bulk = coll.initialize_unordered_bulk_op()
 
   for udoc in udocs:
     rankN = rank_array[index]
     level = count_level(rankN / total)
+
     bulk.find({'_id':udoc['_id']}).update_one(
       {'$set':
         {
@@ -86,6 +89,7 @@ def handle_rank():
     pprint(bwe.details)
 
 def main():
+  options.parse_command_line()
   handle_rank()
 
 if __name__ == '__main__':
