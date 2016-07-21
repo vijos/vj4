70cb35dc9397cc962156c07d0b43a8dfc759d998
Breezewish <me@breeswish.org>
2016-07-22 00:10:32 +0800

    flexbox shortcuts

--- vj4/ui/components/ide/ToolbarComponent.js
+++ vj4/ui/components/ide/ToolbarComponent.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const ToolbarComponent = (props) => (
-  <div className="ide-toolbar">
+  <div className="ide-toolbar flex-row flex-cross-center">
     {props.children}
   </div>
 );
