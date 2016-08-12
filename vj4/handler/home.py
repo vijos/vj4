e12aea935b97ead7b105386b897ca71fbf0c1255
Wende Tan <twd2@163.com>
2016-08-13 00:22:43 +0800

    fix projection

--- vj4/handler/home.py
+++ vj4/handler/home.py
@@ -136,8 +136,9 @@ class HomeMessagesView(base.OperationHandler):
   async def get(self):
     # TODO(iceboy): projection, pagination.
     messages = await message.get_multi(self.user['_id']).sort([('_id', -1)]).to_list(50)
-    await asyncio.gather(user.attach_udocs(messages, 'sender_uid', 'sender_udoc'),
-                         user.attach_udocs(messages, 'sendee_uid', 'sendee_udoc'))
+    await asyncio.gather(
+      user.attach_udocs(messages, 'sender_uid', 'sender_udoc', user.PROJECTION_PUBLIC),
+      user.attach_udocs(messages, 'sendee_uid', 'sendee_udoc', user.PROJECTION_PUBLIC))
     self.json_or_render('home_messages.html', messages=messages)
 
   @base.require_priv(builtin.PRIV_USER_PROFILE)
