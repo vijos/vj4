98e1164b4a444afc48b958d143f9ee55aa64aa9a
Breezewish <me@breeswish.org>
2016-08-13 17:06:07 +0800

    might work?

--- vj4/ui/pages/home_messages.page.js
+++ vj4/ui/pages/home_messages.page.js
@@ -2,18 +2,21 @@ import { NamedPage } from '../misc/PageLoader';
 import loadReactRedux from '../utils/loadReactRedux';
 
 const page = new NamedPage('home_messages', async () => {
-  const SockJs = await System.import('sockjs-client');
-
-  const sock = new SockJs('/home/messages-conn');
-  sock.onmessage = (message) => {
-    // TODO(twd2)
-    alert(message.data);
-  };
-
   async function mountComponent() {
+    const SockJs = await System.import('sockjs-client');
     const { default: MessagePadApp } = await System.import('../components/messagepad');
     const { default: MessagePadReducer } = await System.import('../components/messagepad/reducers');
     const { React, render, Provider, store } = await loadReactRedux(MessagePadReducer);
+
+    const sock = new SockJs('/home/messages-conn');
+    sock.onmessage = (message) => {
+      const msg = JSON.parse(message.data);
+      store.dispatch({
+        type: 'DIALOGUES_MESSAGE_PUSH',
+        payload: msg,
+      });
+    };
+
     render(
       <Provider store={store}>
         <MessagePadApp />
