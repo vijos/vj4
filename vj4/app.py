ee580580d8f945c9f59390ccd0703340c423c7b1
Wende Tan <twd2@163.com>
2016-08-19 03:54:43 +0800

    Feature/domain control (#53)
    * domain control
    
    * my domains!
    
    * my domain ui
    
    * fix ui
    
    * answer: no
    
    * grumble

--- vj4/app.py
+++ vj4/app.py
@@ -50,6 +50,7 @@ class Application(web.Application):
     # Load views.
     from vj4.handler import contest
     from vj4.handler import discussion
+    from vj4.handler import domain
     from vj4.handler import home
     from vj4.handler import judge
     from vj4.handler import main
