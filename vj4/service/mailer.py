1a93037fe660fca1da600f62581f36894ca5bf6a
Luke Lazurite <LukeXuan@users.noreply.github.com>
2016-08-28 02:34:46 +0800

    migrate SMTP library from smtplibaio to aiosmtplib (#64)

--- vj4/service/mailer.py
+++ vj4/service/mailer.py
@@ -1,6 +1,6 @@
 from email.mime import text
 
-import smtplibaio
+import aiosmtplib as asmtp
 
 from vj4.util import argmethod
 from vj4.util import options
@@ -18,12 +18,13 @@ async def send_mail(to: str, subject: str, content: str):
   msg['Subject'] = subject
   msg['From'] = options.options.mail_from
   msg['To'] = to
-  with smtplibaio.SMTP_SSL() as server:
-    await server.connect(host=options.options.smtp_host, port=options.options.smtp_port)
+
+  async with asmtp.SMTP_SSL(host=options.options.smtp_host, port=options.options.smtp_port) as server:
     await server.ehlo()
     await server.login(options.options.smtp_user, options.options.smtp_password)
     await server.sendmail(options.options.mail_from, to, msg.as_string())
-    await server.quit()
+
+
 
 
 if __name__ == '__main__':
