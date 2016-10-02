fd9f9793ba8013dee8980d4c6eecde482c67012d
Wende Tan <twd2@163.com>
2016-10-02 23:11:51 +0800

    grumble

--- vj4/handler/training.py
+++ vj4/handler/training.py
@@ -8,9 +8,7 @@ from vj4.handler import base
 
 @app.route('/training', 'training_main')
 class TrainingMainHandler(base.Handler):
-  @base.require_priv(builtin.PRIV_USER_PROFILE)
   @base.require_perm(builtin.PERM_VIEW_TRAINING)
-  # TODO: permission need to be changed
   async def get(self):
     tdocs = await training.get_list_by_user(self.domain_id, self.user['_id'])
     self.render('training_main.html', tdocs=tdocs)
@@ -52,26 +50,13 @@ class TrainingDetailDemoHandler(base.Handler):
     self.render('training_detail.html')
 
 
-@app.route('/training/detail', 'training_detail')
-class TrainingDetailHandler(base.Handler):
-  @base.require_priv(builtin.PRIV_USER_PROFILE)
-  @base.require_perm(builtin.PERM_VIEW_TRAINING)
-  async def get(self):
-    # TODO: for demo purpose only. remove this handler!
-    self.render('training_detail.html')
-
-
-"""
 @app.route('/training/{tid}', 'training_detail')
 class TrainingDetailHandler(base.Handler):
   @base.require_priv(builtin.PRIV_USER_PROFILE)
   @base.require_perm(builtin.PERM_VIEW_TRAINING)
-  @base.route_argument
-  @base.sanitize
-  async def get(self, *, tid: objectid.ObjectId):
+  async def get(self):
     tdoc = await training.check(self.domain_id, tid, self.user['_id'])
     path_components = self.build_path(
       (self.translate('training_main'), self.reverse_url('training_main')),
       (tdoc['title'], None))
     self.render('training_detail.html', tdoc=tdoc, path_components=path_components)
-"""
--- vj4/model/document.py
+++ vj4/model/document.py
@@ -319,6 +319,13 @@ async def ensure_indexes():
                            ('doc_type', 1),
                            ('rule', 1),
                            ('doc_id', -1)], sparse=True)
+  # for training
+  await coll.ensure_index([('domain_id', 1),
+                           ('doc_type', 1),
+                           ('pids', 1)], sparse=True)
+  await coll.ensure_index([('domain_id', 1),
+                           ('doc_type', 1),
+                           ('require_tids', 1)], sparse=True)
   status_coll = db.Collection('document.status')
   await status_coll.ensure_index([('domain_id', 1),
                                   ('doc_type', 1),
