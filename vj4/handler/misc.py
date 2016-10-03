315759864d2425eb32af40c91c625cc40106c560
Wende Tan <twd2@163.com>
2016-10-03 15:56:52 +0800

    wiki pages

--- vj4/handler/misc.py
+++ vj4/handler/misc.py
@@ -3,6 +3,18 @@ from vj4 import template
 from vj4.handler import base
 
 
+@app.route('/about', 'about')
+class AboutHandler(base.Handler):
+  async def get(self):
+    self.render('about.html')
+
+
+@app.route('/wiki/help', 'wiki_help')
+class AboutHandler(base.Handler):
+  async def get(self):
+    self.render('wiki_help.html')
+
+
 @app.route('/preview', 'preview')
 class PreviewHandler(base.Handler):
   @base.post_argument
