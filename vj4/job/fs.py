ab797d08cd8eba13d2b081dc776913651cbf36c1
Wende Tan <twd2@163.com>
2017-02-19 01:07:46 +0800

    fs job

---
+++ vj4/job/fs.py
@@ -0,0 +1,77 @@
+import asyncio
+import logging
+
+from vj4 import db
+from vj4.model import builtin
+from vj4.model import domain
+from vj4.model import document
+from vj4.model import fs
+from vj4.model.adaptor import userfile
+from vj4.util import argmethod
+from vj4.util import domainjob
+
+
+_logger = logging.getLogger(__name__)
+
+
+@argmethod.wrap
+async def sync_length():
+  _logger.info('Userfile length')
+  coll = db.Collection('document')
+  ufdocs = userfile.get_multi()
+  bulk = coll.initialize_unordered_bulk_op()
+  execute = False
+  _logger.info('Syncing')
+  async for ufdoc in ufdocs:
+    l = 0
+    if ufdoc.get('file_id'):
+      fdoc = await fs.get_meta(ufdoc['file_id'])
+      if fdoc and fdoc.get('length'):
+        l = fdoc['length']
+    bulk.find({'_id': ufdoc['_id']}) \
+        .update_one({'$set': {'length': l}})
+    execute = True
+  if execute:
+    _logger.info('Committing')
+    await bulk.execute()
+
+
+@argmethod.wrap
+async def sync_usage():
+  _logger.info('Userfile length group by user')
+  pipeline = [
+    {
+      '$match': {'domain_id': userfile.STORE_DOMAIN_ID,
+                 'doc_type': document.TYPE_USERFILE}
+    },
+    {
+      '$group': {
+        '_id': '$owner_uid',
+        'usage_userfile': {'$sum': '$length'}
+      }
+    }
+  ]
+  coll = db.Collection('domain.user')
+  await coll.update_many({'domain_id': userfile.STORE_DOMAIN_ID},
+                         {'$set': {'usage_userfile': 0}})
+  bulk = coll.initialize_unordered_bulk_op()
+  execute = False
+  _logger.info('Counting')
+  async for adoc in db.Collection('document').aggregate(pipeline):
+    bulk.find({'domain_id': userfile.STORE_DOMAIN_ID,
+               'uid': adoc['_id']}) \
+        .update_one({'$set': {'usage_userfile': adoc['usage_userfile']}})
+    execute = True
+  if execute:
+    _logger.info('Committing')
+    await bulk.execute()
+
+
+@argmethod.wrap
+async def sync():
+  await sync_length()
+  await sync_usage()
+
+
+if __name__ == '__main__':
+  argmethod.invoke_by_args()
