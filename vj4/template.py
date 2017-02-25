80708de2c07eb2656b9190e5dcdb5f1f44e1afe3
Wende Tan <twd2@163.com>
2017-02-25 19:07:51 +0800

    rewrite format_size

--- vj4/template.py
+++ vj4/template.py
@@ -44,7 +44,7 @@ class Environment(jinja2.Environment):
     self.filters['markdown'] = markdown
     self.filters['json'] = json.encode
     self.filters['gravatar_url'] = gravatar_url
-    self.filters['to_size'] = to_size
+    self.filters['format_size'] = format_size
 
 
 
@@ -116,8 +116,12 @@ def paginate(page, num_pages):
     yield 'last', num_pages
 
 
-def to_size(size_kb, threshold=2048, ndigits=3):
-  if size_kb < threshold:
-    return '{0}KiB'.format(size_kb)
-  else:
-    return '{0}MiB'.format(round(size_kb / 1024, ndigits=ndigits))
+def format_size(size, base=1, ndigits=3):
+  size *= base
+  unit = 1024
+  unit_names = ['Bytes', 'KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB']
+  for unit_name in unit_names:
+    if size < unit:
+      return '{0}{1}'.format(round(size, ndigits=ndigits), unit_name)
+    size /= unit
+  return '{0}{1}'.format(round(size * unit, ndigits=ndigits), unit_names[-1])
