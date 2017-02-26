a3dbcc4edf04ab34ed8c4bcfbe66a97ef51089a0
Breezewish <me@breeswish.org>
2017-02-26 22:29:21 +0800

    update

--- vj4/ui/pages/problem_main.page.js
+++ vj4/ui/pages/problem_main.page.js
@@ -30,7 +30,7 @@ async function updateSelection() {
     if (type === 'subcategory') {
       item = categories[category].children[subcategory];
       setDomSelected(item.$tag, item.select);
-      const selectionName = `${category} ${subcategory}`;
+      const selectionName = `${category},${subcategory}`;
       if (item.select) {
         selections.push(selectionName);
       } else {
@@ -40,11 +40,19 @@ async function updateSelection() {
   });
   dirtyCategories.length = 0;
 
-  // make request according to the selection
-  const requestTags = _.uniq(_.flatten(selections.map(s => s.split(' '))));
-  const resp = await request.get(substitute(decodeURIComponent(Context.getProblemUrl), {
-    category: requestTags.map(tag => encodeURIComponent(tag)).join('+'),
-  }));
+  // a list of categories which subcategory is selected
+  const requestCategoryTags = _.uniq(selections
+    .filter(s => s.indexOf(',') !== -1)
+    .map(s => s.split(',')[0])
+  );
+  // drop the category if its subcategory is selected
+  const requestTags = _.uniq(_.pullAll(selections, requestCategoryTags));
+  const url = substitute(decodeURIComponent(Context.getProblemUrl), {
+    category: requestTags
+      .map(tag => tag.split(',').map(encodeURIComponent).join(','))
+      .join('+'),   // build a beautiful URL
+  });
+  const resp = await request.get(url);
   $('[data-widget-cf-target]').html(resp.html).trigger('vjContentNew');
 }
 
@@ -60,7 +68,6 @@ function buildCategoryFilter() {
     const $categoryTag = $category
       .find('.section__title a')
       .remove()
-      .attr('onclick', 'return false')
       .attr('class', 'widget--category-filter__category-tag');
     const categoryText = $categoryTag.text();
     const $drop = $category
@@ -80,7 +87,6 @@ function buildCategoryFilter() {
         .children('li')
         .attr('class', 'widget--category-filter__subcategory')
         .find('a')
-        .attr('onclick', 'return false')
         .attr('class', 'widget--category-filter__subcategory-tag')
         .attr('data-category', categoryText);
       $subCategoryTags.get().forEach((subCategoryTag) => {
@@ -96,8 +102,11 @@ function buildCategoryFilter() {
       });
     }
   });
-  $(document).on('click', '.widget--category-filter__category-tag', (e) => {
-    const category = $(e.currentTarget).text();
+  $(document).on('click', '.widget--category-filter__category-tag', (ev) => {
+    if (ev.shiftKey || ev.metaKey || ev.ctrlKey) {
+      return;
+    }
+    const category = $(ev.currentTarget).text();
     const treeItem = categories[category];
     // the effect should be cancelSelect if it is shown as selected when clicking
     const shouldSelect = treeItem.$tag.hasClass('selected') ? false : !treeItem.select;
@@ -113,14 +122,19 @@ function buildCategoryFilter() {
       });
     }
     updateSelection();
+    ev.preventDefault();
   });
-  $(document).on('click', '.widget--category-filter__subcategory-tag', (e) => {
-    const subcategory = $(e.currentTarget).text();
-    const category = $(e.currentTarget).attr('data-category');
+  $(document).on('click', '.widget--category-filter__subcategory-tag', (ev) => {
+    if (ev.shiftKey || ev.metaKey || ev.ctrlKey) {
+      return;
+    }
+    const subcategory = $(ev.currentTarget).text();
+    const category = $(ev.currentTarget).attr('data-category');
     const treeItem = categories[category].children[subcategory];
     treeItem.select = !treeItem.select;
     dirtyCategories.push({ type: 'subcategory', subcategory, category });
     updateSelection();
+    ev.preventDefault();
   });
 }
 
