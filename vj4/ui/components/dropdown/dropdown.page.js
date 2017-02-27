d1e44afb4283bf86916f144d6055fd9897d94156
Breezewish <me@breeswish.org>
2017-02-27 22:57:11 +0800

    Dropdown lifecycle

--- vj4/ui/components/dropdown/dropdown.page.js
+++ vj4/ui/components/dropdown/dropdown.page.js
@@ -2,7 +2,7 @@ import { AutoloadPage } from 'vj/misc/PageLoader';
 import Dropdown from './Dropdown';
 
 const dropdownPage = new AutoloadPage('dropdownPage', () => {
-  Dropdown.initAll();
+  Dropdown.registerLifeCycleHooks();
 });
 
 export default dropdownPage;
