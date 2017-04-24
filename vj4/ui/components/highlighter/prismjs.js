0064d26d5b26f142c0d04d051a8a098db5d38f56
iceboy <me@iceboy.org>
2017-04-23 17:38:49 -0700

    use explicit language classes

--- vj4/ui/components/highlighter/prismjs.js
+++ vj4/ui/components/highlighter/prismjs.js
@@ -11,15 +11,6 @@ import 'prismjs/components/prism-php.js';
 import 'prismjs/components/prism-rust.js';
 import 'prismjs/components/prism-haskell.js';
 
-// Set up language aliases.
-const languages = global.Prism.languages;
-languages.cc = languages.cpp;
-languages.pas = languages.pascal;
-languages.py = languages.python;
-languages.py3 = languages.python;
-languages.rs = languages.rust;
-languages.hs = languages.haskell;
-
 const prismjsApiWrap = {
   highlightBlocks: ($dom) => {
     $dom.find('pre code').get().forEach(block => prismjs.highlightElement(block));
