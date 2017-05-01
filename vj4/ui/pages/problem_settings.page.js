ea205db7d12b3f0807704377136efe6bb2683404
Wende Tan <twd2@163.com>
2017-05-01 17:14:05 +0800

    Fix #158 (#182)
    * fix
    
    * try fix #158

--- vj4/ui/pages/problem_settings.page.js
+++ vj4/ui/pages/problem_settings.page.js
@@ -10,6 +10,10 @@ async function handleCategoryClick(ev) {
 
 const page = new NamedPage('problem_settings', async () => {
   $(document).on('click', '.category-a', handleCategoryClick);
+  $(document).on('click', '[name="problem-sidebar__show-category"]', (ev) => {
+    $(ev.currentTarget).hide();
+    $('[name="problem-sidebar__categories"]').show();
+  });
 });
 
 export default page;
