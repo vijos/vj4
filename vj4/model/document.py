vijos/vj4

commit 64e55deae885dc474d2a6b41dda07e696a2d503b

discussion update_at

vj4/model/document.py
@@ -125,6 +125,19 @@ async def inc(domain_id: str, doc_type: int, doc_id: convert_doc_id, key: str, v
 
 
 @argmethod.wrap
+async def inc_and_set(domain_id: str, doc_type: int, doc_id: convert_doc_id,
+                      inc_key: str, inc_value: int, set_key: str, set_value: lambda _: _):
+  coll = db.Collection('document')
+  doc = await coll.find_and_modify(query={'domain_id': domain_id,
+                                          'doc_type': doc_type,
+                                          'doc_id': doc_id},
+                                   update={'$inc': {inc_key: inc_value},
+                                           '$set': {set_key: set_value}},
+                                   new=True)
+  return doc
+
+
+@argmethod.wrap
 async def push(domain_id: str, doc_type: int, doc_id: convert_doc_id, key: str,
                content: str, owner_uid: int, **kwargs):
   coll = db.Collection('document')
@@ -303,11 +316,19 @@ async def ensure_indexes():
   await coll.ensure_index([('domain_id', 1),
                            ('doc_type', 1),
                            ('doc_id', 1)], unique=True)
+  # for problem solution
   await coll.ensure_index([('domain_id', 1),
                            ('doc_type', 1),
                            ('parent_doc_type', 1),
                            ('parent_doc_id', 1),
                            ('vote', -1),
+                           ('doc_id', -1)], sparse=True)
+  # for discussion
+  await coll.ensure_index([('domain_id', 1),
+                           ('doc_type', 1),
+                           ('parent_doc_type', 1),
+                           ('parent_doc_id', 1),
+                           ('update_at', -1),
                            ('doc_id', -1)], sparse=True)
   # hidden doc
   await coll.ensure_index([('domain_id', 1),
@@ -331,12 +352,12 @@ async def ensure_indexes():
                                   ('status', 1),
                                   ('rid', 1),
                                   ('rp', 1)], sparse=True)
-  # contest rule OI
+  # for contest rule OI
   await status_coll.ensure_index([('domain_id', 1),
                                   ('doc_type', 1),
                                   ('doc_id', 1),
                                   ('score', -1)], sparse=True)
-  # contest rule ACM
+  # for contest rule ACM
   await status_coll.ensure_index([('domain_id', 1),
                                   ('doc_type', 1),
                                   ('doc_id', 1),

vj4/handler/base.py
@@ -54,6 +54,7 @@ class HandlerBase(setting.SettingMixin):
     self.timezone = pytz.timezone(self.get_setting('timezone'))
     self.translate = locale.get_translate(self.view_lang)
     self.datetime_span = functools.partial(_datetime_span, timezone=self.timezone)
+    self.datetime_stamp = _datetime_stamp
     self.reverse_url = functools.partial(_reverse_url, domain_id=self.domain_id)
     self.build_path = functools.partial(_build_path, domain_id=self.domain_id,
                                         domain_name=self.domain['name'])
@@ -184,6 +185,7 @@ class HandlerBase(setting.SettingMixin):
       kwargs['path_components'] = self.build_path((self.translate(self.NAME), None))
     kwargs['reverse_url'] = self.reverse_url
     kwargs['datetime_span'] = self.datetime_span
+    kwargs['datetime_stamp'] = self.datetime_stamp
     return template.Environment().get_template(template_name).render(kwargs)
 
 
@@ -338,6 +340,13 @@ def _datetime_span(dt, relative=True, format='%Y-%m-%d %H:%M:%S', timezone=pytz.
           ' relative' if relative else '',
           calendar.timegm(dt.utctimetuple()),
           dt.astimezone(timezone).strftime(format)))
+
+
+@functools.lru_cache()
+def _datetime_stamp(dt):
+  if not dt.tzinfo:
+    dt = dt.replace(tzinfo=pytz.utc)
+  return calendar.timegm(dt.utctimetuple())
 
 
 # Decorators

vj4/model/adaptor/discussion.py
@@ -1,6 +1,6 @@
 import asyncio
 import collections
-
+import datetime
 from bson import objectid
 from pymongo import errors
 
@@ -127,6 +127,7 @@ async def add(domain_id: str, node_or_dtuple: str, owner_uid: int, title: str, c
       raise error.DiscussionNodeNotFoundError(domain_id, node_or_dtuple)
   return await document.add(domain_id, content, owner_uid, document.TYPE_DISCUSSION,
                             title=title, num_replies=0, views=0, **flags,
+                            update_at=datetime.datetime.utcnow(),
                             parent_doc_type=vnode['doc_type'], parent_doc_id=vnode['doc_id'])
 
 
@@ -141,6 +142,7 @@ async def edit(domain_id: str, did: document.convert_doc_id, **kwargs):
 
 @argmethod.wrap
 async def delete(domain_id: str, did: document.convert_doc_id):
+  # TODO(twd2): delete status?
   await document.delete(domain_id, document.TYPE_DISCUSSION, did)
   await document.delete_multi(domain_id, document.TYPE_DISCUSSION_REPLY,
                               parent_doc_type=document.TYPE_DISCUSSION,
@@ -164,10 +166,11 @@ async def count(domain_id: str, **kwargs):
 @argmethod.wrap
 def get_multi(domain_id: str, *, fields=None, **kwargs):
   return document.get_multi(domain_id=domain_id,
-                                  doc_type=document.TYPE_DISCUSSION,
-                                  fields=fields,
-                                  **kwargs) \
-                 .sort([('doc_id', -1)])
+                            doc_type=document.TYPE_DISCUSSION,
+                            fields=fields,
+                            **kwargs) \
+                 .sort([('update_at', -1),
+                        ('doc_id', -1)])
 
 
 @argmethod.wrap
@@ -176,7 +179,8 @@ async def add_reply(domain_id: str, did: document.convert_doc_id, owner_uid: int
   drdoc, _ = await asyncio.gather(
     document.add(domain_id, content, owner_uid, document.TYPE_DISCUSSION_REPLY,
                  parent_doc_type=document.TYPE_DISCUSSION, parent_doc_id=did),
-    document.inc(domain_id, document.TYPE_DISCUSSION, did, 'num_replies', 1))
+    document.inc_and_set(domain_id, document.TYPE_DISCUSSION, did,
+                         'num_replies', 1, 'update_at', datetime.datetime.utcnow()))
   return drdoc
 
 
@@ -221,8 +225,11 @@ async def get_list_reply(domain_id: str, did: document.convert_doc_id, *, fields
 async def add_tail_reply(domain_id: str, drid: document.convert_doc_id,
                          owner_uid: int, content: str):
   validator.check_content(content)
-  return await document.push(domain_id, document.TYPE_DISCUSSION_REPLY, drid,
-                             'reply', content, owner_uid)
+  drdoc, sid = await document.push(domain_id, document.TYPE_DISCUSSION_REPLY, drid,
+                                   'reply', content, owner_uid)
+  await document.set(domain_id, document.TYPE_DISCUSSION, drdoc['parent_doc_id'],
+                     update_at=datetime.datetime.utcnow())
+  return drdoc, sid
 
 
 @argmethod.wrap

vj4/ui/templates/partials/discussion_list.html
@@ -15,7 +15,7 @@
               </div>
             </div>
             <div class="media__body top">
-              <h1 class="discussion__title"><a href="{{ reverse_url('discussion_detail', did=ddoc['doc_id']) }}" data-emoji-enabled>{{ ddoc['title'] }}</a></h1>
+              <h1 class="discussion__title"><a href="{{ reverse_url('discussion_detail', did=ddoc['doc_id']) }}?{{ datetime_stamp(ddoc['update_at']) if ddoc['update_at'] else '' }}" data-emoji-enabled>{{ ddoc['title'] }}</a></h1>
               <ul class="supplementary list">
                 <li>
                   <a class="v-center discussion-node-tag" href="{{ vj4.handler.discussion.node_url(handler, 'discussion_node', vj4.model.adaptor.discussion.node_id(ddoc)) }}">{% if ddoc['parent_doc_type'] == vj4.model.document.TYPE_DISCUSSION_NODE %}<span class="icon icon-tag"></span>{% elif ddoc['parent_doc_type'] == vj4.model.document.TYPE_CONTEST %}<span class="icon icon-award"></span>{% else %}<span class="icon icon-flag"></span>{% endif %}{{ vndict[vj4.model.adaptor.discussion.node_id(ddoc)]['title'] }}</a>

commit 9504a0c9dd0e191d6dbb58fa276772306f8246b3

fix performance bug introduced in 64e55deae885dc474d2a6b41dda07e696a2d503b

vj4/model/document.py
@@ -376,6 +376,10 @@ async def ensure_indexes():
   # for discussion
   await coll.create_index([('domain_id', 1),
                            ('doc_type', 1),
+                           ('update_at', -1),
+                           ('doc_id', -1)], sparse=True)
+  await coll.create_index([('domain_id', 1),
+                           ('doc_type', 1),
                            ('parent_doc_type', 1),
                            ('parent_doc_id', 1),
                            ('update_at', -1),