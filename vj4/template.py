9cc74d539c7fd5ffb7dce27546e7707d1f6c1c92
iceboy <me@iceboy.org>
2017-05-04 14:00:00 +0800

    partially fix twd2 (#187)
    * partially fix twd2
    
    * partially fix more twd2
    
    * fix test

--- vj4/template.py
+++ vj4/template.py
@@ -59,27 +59,18 @@ MARKDOWN_RENDER_FLAGS = (hoedown.HTML_ESCAPE |  # Escape all HTML.
                          hoedown.HTML_HARD_WRAP)  # Render each linebreak as <br>.
 
 
-MARKDOWN_PRERENDER = [
-  (r'\(vijos\:\/\/fs\/([0-9a-f]{40,})\)',
-   lambda m: '(' + options.cdn_prefix.rstrip('/') + '/fs/' + m.group(1) + ')')
-   # TODO(twd2): reverse_url
-]
-MARKDOWN_PRERENDER_COMPILED = []
-
-for regex, repl in MARKDOWN_PRERENDER:
-  MARKDOWN_PRERENDER_COMPILED.append((re.compile(regex), repl))
+FS_RE = re.compile(r'\(vijos\:\/\/fs\/([0-9a-f]{40,})\)')
 
 
-def prerender_markdown(text):
-  for regex, repl in MARKDOWN_PRERENDER_COMPILED:
-    text = regex.sub(repl, text)
-  return text
+def fs_replace(m):
+  # TODO(twd2): reverse_url
+  return '(' + options.cdn_prefix.rstrip('/') + '/fs/' + m.group(1) + ')'
 
 
 def markdown(text):
-  return markupsafe.Markup(
-      hoedown.html(prerender_markdown(text), extensions=MARKDOWN_EXTENSIONS,
-                                             render_flags=MARKDOWN_RENDER_FLAGS))
+  text = FS_RE.sub(fs_replace, text)
+  return markupsafe.Markup(hoedown.html(
+      text, extensions=MARKDOWN_EXTENSIONS, render_flags=MARKDOWN_RENDER_FLAGS))
 
 
 def gravatar_url(gravatar, size=200):
