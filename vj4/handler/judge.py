8ce78700d6ff193bf3744e894156f6ef7ffb6767
iceboy <me@iceboy.org>
2017-05-07 13:16:53 -0700

    fix

--- vj4/handler/judge.py
+++ vj4/handler/judge.py
@@ -193,6 +193,9 @@ class JudgeNotifyConnection(base.Connection):
       if 'progress' in kwargs:
         update.setdefault('$set', {})['progress'] = float(kwargs['progress'])
       rdoc = await record.next_judge(rid, self.user['_id'], self.id, **update)
+      if not rdoc:
+        await self.channel.basic_client_ack(tag)
+        return
       bus.publish_throttle('record_change', rdoc, rdoc['_id'])
     elif key == 'end':
       rid = self.rids.pop(tag)
@@ -202,6 +205,9 @@ class JudgeNotifyConnection(base.Connection):
                                                       int(kwargs['time_ms']),
                                                       int(kwargs['memory_kb'])),
                                      self.channel.basic_client_ack(tag))
+      if not rdoc:
+        await self.channel.basic_client_ack(tag)
+        return
       await _post_judge(self, rdoc)
     elif key == 'nack':
       await self.channel.basic_client_nack(tag)
