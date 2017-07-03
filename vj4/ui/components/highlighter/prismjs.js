6e94e6d2127916d491da7597d2bee3b9c1b41d36
Breezewish <me@breeswish.org>
2017-07-04 00:30:02 +0800

    Fix #222

--- vj4/ui/components/highlighter/prismjs.js
+++ vj4/ui/components/highlighter/prismjs.js
@@ -1,3 +1,13 @@
+/*
+
+To add a new language to highlight:
+1. Add new import statement in this file
+2. Add new import statement in `components/cmeditor/vjcmeditor.js`
+3. Add new import statement in `components/scratchpad/ScratchpadEditorContainer.js`
+4. Add new meta data in `components/highlighter/meta.js`
+
+ */
+
 import Prism from 'prismjs/components/prism-core';
 import 'prismjs/components/prism-clike';
 import 'prismjs/components/prism-c';
@@ -15,6 +25,24 @@ import 'prismjs/plugins/line-numbers/prism-line-numbers';
 
 import Clipboard from 'clipboard';
 import Notification from 'vj/components/notification';
+
+import languageMeta from './meta';
+
+const languageExtMap = {};
+
+// Map possible language names to Prism language name
+languageMeta.forEach((meta) => {
+  for (let i = 0; i < meta.ext.length; ++i) {
+    if (Prism.languages[meta.ext[i]] !== undefined) {
+      // eslint-disable-next-line no-param-reassign
+      meta.target = meta.ext[i];
+      break;
+    }
+  }
+  meta.ext.forEach((ext) => {
+    languageExtMap[ext] = meta.target;
+  });
+});
 
 // Copy to Clipboard
 Prism.plugins.toolbar.registerButton('copy-to-clipboard', (env) => {
@@ -38,6 +66,15 @@ const prismjsApiWrap = {
       $pre.addClass('syntax-hl');
       if ($pre.closest('[data-syntax-hl-show-line-number]')) {
         $pre.addClass('line-numbers');
+      }
+      // try to map the language name
+      const language = $(code).attr('class');
+      const m = (language || '').trim().match(/^language-(.+)$/);
+      if (m && m[1]) {
+        const languageName = m[1].toLowerCase();
+        if (languageExtMap[languageName]) {
+          $(code).attr('class', `language-${languageExtMap[languageName]}`);
+        }
       }
       Prism.highlightElement(code);
     });
