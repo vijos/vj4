aff31444f41120df5d268c7c496b2993c68c73ad
Breezewish <me@breeswish.org>
2017-08-13 17:08:07 +0800

    Show relative time for dynamic added records

--- vj4/ui/pages/record_main.page.js
+++ vj4/ui/pages/record_main.page.js
@@ -10,12 +10,15 @@ const page = new NamedPage('record_main', async () => {
 
   sock.onmessage = (message) => {
     const msg = JSON.parse(message.data);
-    const newTr = $(msg.html);
-    const oldTr = $(`.record_main__table tr[data-rid="${newTr.attr('data-rid')}"]`);
-    if (oldTr.length) {
-      dd.apply(oldTr[0], dd.diff(oldTr[0], newTr[0]));
+    const $newTr = $(msg.html);
+    const $oldTr = $(`.record_main__table tr[data-rid="${$newTr.attr('data-rid')}"]`);
+    if ($oldTr.length) {
+      $oldTr.trigger('vjContentRemove');
+      dd.apply($oldTr[0], dd.diff($oldTr[0], $newTr[0]));
+      $oldTr.trigger('vjContentNew');
     } else {
-      $('.record_main__table tbody').prepend(newTr);
+      $('.record_main__table tbody').prepend($newTr);
+      $newTr.trigger('vjContentNew');
     }
   };
   UserSelectAutoComplete.getOrConstruct($('.filter-user [name="uid_or_name"]'), {
