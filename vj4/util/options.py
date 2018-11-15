a554b8a07be6968a7ee23f62a62cc1bf9c86b0ac
Soha Jin <soha@lohu.info>
2018-11-15 19:37:50 +0800

    Add `VJ_CMDLINE_FLAGS` for setting bool flags

--- vj4/util/options.py
+++ vj4/util/options.py
@@ -22,10 +22,14 @@ class Options(object):
   def __getattr__(self, item):
     if self._dirty:
       args_to_parse = []
-      for k,v in os.environ.items(): # using environments start with `VJ_` as arguments
+      for k, v in os.environ.items(): # using environments start with `VJ_` as arguments
         if k.startswith("VJ_"):
-          args_to_parse.append('--' + k[3:].lower().replace('_','-'))
-          args_to_parse.append(v)
+          if k == 'VJ_CMDLINE_FLAGS': # for setting bool flags like `--debug` or `--no-pretty`
+            for v in v.split(' '):
+              args_to_parse.append(v)
+          else:
+            args_to_parse.append('--' + k[3:].lower().replace('_','-'))
+            args_to_parse.append(v)
       for v in sys.argv[1:]:
         args_to_parse.append(v) # cmdline args can override one in env
 
