3d24aefc3a147c6fcb3aae6beddb8f2c0530ab0d
iceboy <me@iceboy.org>
2018-12-05 20:12:57 -0800

    support mongodb authentication

--- vj4/db.py
+++ vj4/db.py
@@ -1,15 +1,29 @@
 import aiomongo
 import functools
+import urllib.parse
 
 from vj4.util import options
 
 options.define('db_host', default='localhost', help='Database hostname or IP address.')
 options.define('db_name', default='test', help='Database name.')
+options.define('db_username', default='', help='Database username.')
+options.define('db_password', default='', help='Database password.')
+
+
+def _escape(s):
+  return urllib.parse.quote(s, safe='')
 
 
 async def init():
   global _client, _db
-  _client = await aiomongo.create_client('mongodb://' + options.db_host)
+  if not options.db_username:
+    uri = 'mongodb://{}'.format(_escape(options.db_host))
+  else:
+    uri = 'mongodb://{}:{}@{}/?authSource={}'.format(_escape(options.db_username),
+                                                     _escape(options.db_password),
+                                                     _escape(options.db_host),
+                                                     _escape(options.db_name))
+  _client = await aiomongo.create_client(uri)
   _db = _client.get_database(options.db_name)
 
 
