1e5c391fe6bde6b84c4eb3187c6e7fce81608af5
Soha Jin <soha@lohu.info>
2018-12-08 13:35:15 +0800

    use `urllib.parse.urlunsplit` to construct an MongoURL

--- vj4/db.py
+++ vj4/db.py
@@ -8,7 +8,8 @@ options.define('db_host', default='localhost', help='Database hostname or IP add
 options.define('db_name', default='test', help='Database name.')
 options.define('db_username', default='', help='Database username.')
 options.define('db_password', default='', help='Database password.')
-options.define('db_auth', default='', help='Database name which stores the user\'s credential.')
+options.define('db_auth_source', default='',
+               help='Database name associated with the user\'s credential.')
 
 
 async def init():
@@ -17,14 +18,21 @@ async def init():
   def escape(s):
     return urllib.parse.quote(s, safe='')
 
-  if not options.db_username:
-    uri = 'mongodb://{}'.format(escape(options.db_host))
-  else:
-    uri = 'mongodb://{}:{}@{}/?authSource={}'.format(escape(options.db_username),
-                                                     escape(options.db_password),
-                                                     escape(options.db_host),
-                                                     escape(options.db_auth if options.db_auth
-                                                       else options.db_name))
+  url_parts = ['mongodb',
+               escape(options.db_host),
+               '/{}'.format(escape(options.db_name)),
+               '', '']
+  if options.db_username:  # add credentials
+    url_parts[1] = ('{}:{}@'.format(escape(options.db_username), escape(options.db_password)))\
+                   + url_parts[1]
+
+  url_query_list = []
+  if options.db_auth_source:
+    url_query_list.append('authSource={}'.format(escape(options.db_auth_source)))
+
+  url_parts[3]='&'.join(url_query_list)  # merge queries
+  uri = urllib.parse.urlunsplit(tuple(url_parts))
+
   _client = await aiomongo.create_client(uri)
   _db = _client.get_database(options.db_name)
 
