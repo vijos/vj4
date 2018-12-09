31c80e63788d8a150853aa6455f7a686cec3379b
Soha Jin <soha@lohu.info>
2018-12-09 14:49:57 +0800

    Option for changing MongoDB's auth source. (#438)

--- vj4/db.py
+++ vj4/db.py
@@ -1,30 +1,36 @@
 import aiomongo
 import functools
-import urllib.parse
+import yarl
 
 from vj4.util import options
 
 options.define('db_host', default='localhost', help='Database hostname or IP address.')
+options.define('db_port', default=27017, help='Database port.')
 options.define('db_name', default='test', help='Database name.')
 options.define('db_username', default='', help='Database username.')
 options.define('db_password', default='', help='Database password.')
+options.define('db_auth_source', default='',
+               help='Database name associated with the user\'s credential.')
 
 
 async def init():
   global _client, _db
 
-  def escape(s):
-    return urllib.parse.quote(s, safe='')
+  url_parts = {
+    'scheme': 'mongodb',
+    'host': options.db_host,
+    'path': options.db_name,
+    'port': options.db_port,
+    'user': options.db_username,
+    'password': options.db_password,
+    'query': {},
+  }
+  if options.db_auth_source:
+    url_parts['query']['authSource'] = options.db_auth_source
+  url = yarl.URL.build(**url_parts)
 
-  if not options.db_username:
-    uri = 'mongodb://{}'.format(escape(options.db_host))
-  else:
-    uri = 'mongodb://{}:{}@{}/?authSource={}'.format(escape(options.db_username),
-                                                     escape(options.db_password),
-                                                     escape(options.db_host),
-                                                     escape(options.db_name))
-  _client = await aiomongo.create_client(uri)
-  _db = _client.get_database(options.db_name)
+  _client = await aiomongo.create_client(str(url))
+  _db = _client.get_default_database()
 
 
 @functools.lru_cache()
