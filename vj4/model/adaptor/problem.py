vijos/vj4

commit 684618f8f1a888c5b006d609e81d621b8b130a55

Copy Problem (#495)

vj4/model/adaptor/problem.py
@@ -36,13 +36,31 @@ def get_categories():
 @argmethod.wrap
 async def add(domain_id: str, title: str, content: str, owner_uid: int,
               pid: document.convert_doc_id=None, data: objectid.ObjectId=None,
-              category: list=[], hidden: bool=False):
+              category: list=[], tag: list=[], hidden: bool=False):
   validator.check_title(title)
   validator.check_content(content)
   pid = await document.add(domain_id, content, owner_uid, document.TYPE_PROBLEM,
-                           pid, title=title, data=data, category=category,
+                           pid, title=title, data=data, category=category, tag=tag,
                            hidden=hidden, num_submit=0, num_accept=0)
   await domain.inc_user(domain_id, owner_uid, num_problems=1)
+  return pid
+
+
+async def copy(pdoc, dest_domain_id: str, owner_uid: int,
+               pid: document.convert_doc_id=None, hidden: bool=False):
+  # This copies contents only, data will be referenced to the source problem.
+  data = pdoc['data']
+  src_domain_id, src_pid = pdoc['domain_id'], pdoc['doc_id']
+  if type(data) is objectid.ObjectId:
+    data = { 'domain': src_domain_id, 'pid': src_pid }
+  elif type(data) is dict:
+    src_domain_id, src_pid = data['domain'], data['pid']
+
+  pid = await add(domain_id=dest_domain_id, owner_uid=owner_uid,
+                  title=pdoc['title'], content=pdoc['content'],
+                  pid=pid, hidden=hidden, category=pdoc['category'],
+                  data=data, tag=pdoc.get('tag', []))
+  await document.inc(src_domain_id, document.TYPE_PROBLEM, src_pid, 'num_be_copied', 1)
   return pid
 
 
@@ -262,11 +280,16 @@ def delete_solution_reply(domain_id: str, psid: document.convert_doc_id, psrid: 
   return document.delete_sub(domain_id, document.TYPE_PROBLEM_SOLUTION, psid, 'reply', psrid)
 
 
-async def get_data(domain_id, pid):
-  pdoc = await get(domain_id, pid)
-  if not pdoc.get('data', None):
-    raise error.ProblemDataNotFoundError(domain_id, pid)
-  return await fs.get_meta(pdoc['data'])
+async def get_data(pdoc):
+  data = pdoc.get('data', None)
+  if not data:
+    return None
+  if type(data) is dict:
+    upper_pdoc = await get(data['domain'], data['pid'])
+    data = upper_pdoc['data']
+    if not data:
+      return None
+  return await fs.get_meta(data)
 
 
 @argmethod.wrap
@@ -294,9 +317,10 @@ async def get_data_list(last: int):
   pdocs = coll.find({'doc_type': document.TYPE_PROBLEM})
   pids = []  # with domain_id
   async for pdoc in pdocs:
-    if 'data' not in pdoc or not pdoc['data']:
+    data = await get_data(pdoc)
+    if not data:
       continue
-    date = await fs.get_datetime(pdoc['data'])
+    date = data['uploadDate']
     if not date:
       continue
     if last_datetime < date:

vj4/ui/templates/problem_detail.html
@@ -1,5 +1,32 @@
 {% extends "layout/basic.html" %}
 {% block content %}
+<div style="display: none" class="dialog__body--copy-to">
+  <div>
+    <div class="row"><div class="columns">
+      <h1>{{ _('Copy Problem To') }}</h1>
+    </div></div>
+    <div class="row">
+      <div class="medium-12 columns">
+        <label>
+          {{ _('Destination Domain') }}
+          <input name="domain_id" placeholder="{{ _('Domain ID') }}" type="text" class="textbox" autocomplete="off" data-autofocus>
+        </label>
+      </div>
+    </div>
+    <div class="row">
+      <div class="medium-12 columns">
+        <label>
+          <label class="checkbox">
+            <input type="checkbox" name="numeric_pid" checked>{{ _('Numeric PID') }}
+          </label>
+          <label class="checkbox">
+            <input type="checkbox" name="hidden">{{ _('Hidden') }}
+          </label>
+        </label>
+      </div>
+    </div>
+  </div>
+</div>
 <script>
   var Context = {{ {
     'problemId': pdoc['doc_id'],
@@ -23,6 +50,11 @@
         {% if not pdoc['data'] %}
           <blockquote class="warn">
             <p>{{ _('No dataset at present.') }}</p>
+          </blockquote>
+        {% endif %}
+        {% if pdoc['data'] is mapping %}
+          <blockquote>
+            <p>{{ _('Test data comes from') }} <a href="{{ reverse_url('problem_detail', domain_id=pdoc['data']['domain'], pid=pdoc['data']['pid']) }}">{{ pdoc['data']['domain'] }}/{{ pdoc['data']['pid'] }}</a></p>
           </blockquote>
         {% endif %}
         {% if tdoc %}

vj4/ui/templates/problem_upload.html
@@ -1,8 +1,12 @@
 {% extends 'layout/simple.html' %}
 {% block body %}
 <form method="post" enctype="multipart/form-data">
+{% if pdoc['data'] is mapping %}
+<p>{{ _('Test data comes from') }} <a href="{{ reverse_url('problem_detail', domain_id=pdoc['data']['domain'], pid=pdoc['data']['pid']) }}">{{ pdoc['data']['domain'] }}/{{ pdoc['data']['pid'] }}</a></p>
+{% else %}
 <p>{{ _('Current dataset: {0}').format(pdoc['data']|default(_('(None)'))) }}</p>
 <p>{{ _('MD5: {0}').format(md5|default(_('(None)'))) }}</p>
+{% endif %}
 <p>
 {{ _('New dataset') }}:
 <input type="hidden" name="csrf_token" value="{{ handler.csrf_token }}">

vj4/handler/problem.py
@@ -25,6 +25,7 @@ from vj4.model.adaptor import training
 from vj4.service import bus
 from vj4.util import pagination
 from vj4.util import options
+from vj4.util import misc
 
 
 async def render_or_json_problem_list(self, page, ppcount, pcount, pdocs,
@@ -185,7 +186,7 @@ class ProblemCategoryRandomHandler(base.Handler):
 
 
 @app.route('/p/{pid:-?\d+|\w{24}}', 'problem_detail')
-class ProblemDetailHandler(base.Handler):
+class ProblemDetailHandler(base.OperationHandler):
   async def _get_related_trainings(self, pid):
     if self.has_perm(builtin.PERM_VIEW_TRAINING):
       return await training.get_multi(self.domain_id, **{'dag.pids': pid}).to_list()
@@ -220,6 +221,35 @@ class ProblemDetailHandler(base.Handler):
     self.render('problem_detail.html', pdoc=pdoc, udoc=udoc, dudoc=dudoc,
                 tdocs=tdocs, ctdocs=ctdocs, htdocs=htdocs,
                 page_title=pdoc['title'], path_components=path_components)
+
+  @base.require_priv(builtin.PRIV_USER_PROFILE)
+  @base.require_perm(builtin.PERM_VIEW_PROBLEM)
+  @base.require_csrf_token
+  @base.route_argument
+  @base.sanitize
+  @base.limit_rate('copy_problem', 60, 100)
+  async def post_copy_to_domain(self, *,
+                                pid: document.convert_doc_id, dest_domain_id: str,
+                                numeric_pid: bool=False, hidden: bool=False):
+    uid = self.user['_id']
+    pdoc = await problem.get(self.domain_id, pid, uid)
+    if pdoc.get('hidden', False):
+      self.check_perm(builtin.PERM_VIEW_PROBLEM_HIDDEN)
+    ddoc, dudoc = await asyncio.gather(domain.get(dest_domain_id),
+                                       domain.get_user(dest_domain_id, uid))
+    if not dudoc:
+      dudoc = {}
+    if not self.dudoc_has_perm(dudoc=dudoc, perm=builtin.PERM_CREATE_PROBLEM, ddoc=ddoc, udoc=self.user):
+      # TODO: This is the destination domain's PermissionError.
+      raise error.PermissionError(builtin.PERM_CREATE_PROBLEM)
+
+    pid = None
+    if numeric_pid:
+      pid = await domain.inc_pid_counter(dest_domain_id)
+    pid = await problem.copy(pdoc, dest_domain_id, uid, pid, hidden)
+
+    new_url = self.reverse_url('problem_settings', pid=pid, domain_id=dest_domain_id)
+    self.json_or_redirect(new_url, new_problem_url=new_url)
 
 
 @app.route('/p/{pid}/submit', 'problem_submit')
@@ -523,10 +553,14 @@ class ProblemDataHandler(base.Handler):
     # domain administrators will have PERM_READ_PROBLEM_DATA,
     # problem owner will have PERM_READ_PROBLEM_DATA_SELF.
     pdoc = await problem.get(self.domain_id, pid)
+    if type(pdoc['data']) is dict:
+      return self.redirect(self.reverse_url('problem_data',
+                           domain_id=pdoc['data']['domain'],
+                           pid=pdoc['data']['pid']))
     if (not self.own(pdoc, builtin.PERM_READ_PROBLEM_DATA_SELF)
         and not self.has_perm(builtin.PERM_READ_PROBLEM_DATA)):
       self.check_priv(builtin.PRIV_READ_PROBLEM_DATA)
-    fdoc = await problem.get_data(self.domain_id, pid)
+    fdoc = await problem.get_data(pdoc)
     if not fdoc:
       raise error.ProblemDataNotFoundError(self.domain_id, pid)
     self.redirect(options.cdn_prefix.rstrip('/') + \
@@ -553,6 +587,61 @@ class ProblemCreateHandler(base.Handler):
     pid = await problem.add(self.domain_id, title, content, self.user['_id'],
                             hidden=hidden, pid=pid)
     self.json_or_redirect(self.reverse_url('problem_settings', pid=pid))
+
+
+@app.route('/p/copy', 'problem_copy')
+class ProblemCopyHandler(base.Handler):
+  MAX_PROBLEMS_PER_REQUEST = 20
+
+  @base.require_priv(builtin.PRIV_USER_PROFILE)
+  @base.require_perm(builtin.PERM_CREATE_PROBLEM)
+  async def get(self):
+    self.render('problem_copy.html')
+
+  @base.require_priv(builtin.PRIV_USER_PROFILE)
+  @base.require_perm(builtin.PERM_CREATE_PROBLEM)
+  @base.post_argument
+  @base.require_csrf_token
+  @base.sanitize
+  @base.limit_rate('copy_problems', 30, 10)
+  async def post(self, *, src_domain_id: str, src_pids: str,
+                 numeric_pid: bool=False, hidden: bool=False):
+    src_ddoc, src_dudoc = await asyncio.gather(domain.get(src_domain_id),
+                                               domain.get_user(src_domain_id, self.user['_id']))
+    if not src_dudoc:
+      src_dudoc = {}
+    if not self.dudoc_has_perm(ddoc=src_ddoc, dudoc=src_dudoc, udoc=self.user,
+                               perm=builtin.PERM_VIEW_PROBLEM):
+      # TODO: This is the source domain's PermissionError.
+      raise error.PermissionError(builtin.PERM_VIEW_PROBLEM)
+
+    src_pids = misc.dedupe(map(document.convert_doc_id, src_pids.replace('\r\n', '\n').split('\n')))
+    if len(src_pids) > self.MAX_PROBLEMS_PER_REQUEST:
+      raise error.BatchCopyLimitExceededError(self.MAX_PROBLEMS_PER_REQUEST, len(src_pids))
+    pdocs = await problem.get_multi(domain_id=src_domain_id, doc_id={'$in': src_pids}) \
+      .sort('doc_id', 1) \
+      .to_list()
+
+    exist_pids = [pdoc['doc_id'] for pdoc in pdocs]
+    if len(src_pids) != len(exist_pids):
+      for pid in src_pids:
+        if pid not in exist_pids:
+          raise error.ProblemNotFoundError(src_domain_id, pid)
+
+    for pdoc in pdocs:
+      if pdoc.get('hidden', False):
+        if not self.dudoc_has_perm(ddoc=src_ddoc, dudoc=src_dudoc, udoc=self.user,
+                                   perm=builtin.PERM_VIEW_PROBLEM_HIDDEN):
+          # TODO: This is the source domain's PermissionError.
+          raise error.PermissionError(builtin.PERM_VIEW_PROBLEM_HIDDEN)
+
+    for pdoc in pdocs:
+      pid = None
+      if numeric_pid:
+        pid = await domain.inc_pid_counter(self.domain_id)
+      await problem.copy(pdoc, self.domain_id, self.user['_id'], pid, hidden)
+
+    self.redirect(self.reverse_url('problem_main'))
 
 
 @app.route('/p/{pid}/edit', 'problem_edit')
@@ -661,7 +750,7 @@ class ProblemUploadHandler(base.Handler):
     if (not self.own(pdoc, builtin.PERM_READ_PROBLEM_DATA_SELF)
         and not self.has_perm(builtin.PERM_READ_PROBLEM_DATA)):
       self.check_priv(builtin.PRIV_READ_PROBLEM_DATA)
-    md5 = await fs.get_md5(pdoc.get('data'))
+    md5 = await fs.get_md5(await problem.get_data(pdoc))
     self.render('problem_upload.html', pdoc=pdoc, md5=md5)
 
   @base.require_priv(builtin.PRIV_USER_PROFILE)
@@ -676,7 +765,7 @@ class ProblemUploadHandler(base.Handler):
     if (not self.own(pdoc, builtin.PERM_READ_PROBLEM_DATA_SELF)
         and not self.has_perm(builtin.PERM_READ_PROBLEM_DATA)):
       self.check_priv(builtin.PRIV_READ_PROBLEM_DATA)
-    if pdoc.get('data'):
+    if pdoc.get('data') and type(pdoc['data']) is objectid.ObjectId:
       await fs.unlink(pdoc['data'])
     await problem.set_data(self.domain_id, pid, file)
     self.json_or_redirect(self.url)

vj4/error.py
@@ -368,6 +368,12 @@ class NoProblemError(NotFoundError):
     return 'No problem.'
 
 
+class BatchCopyLimitExceededError(ForbiddenError):
+  @property
+  def message(self):
+    return 'Only {0} problems can be copied in one request, got {1}.'
+
+
 class UpgradeLockAcquireError(Error):
   @property
   def message(self):

vj4/handler/base.py
@@ -88,12 +88,12 @@ class HandlerBase(setting.SettingMixin):
     if not self.has_priv(priv):
       raise error.PrivilegeError(priv)
 
-  def dudoc_has_perm(self, udoc, dudoc, perm):
+  def dudoc_has_perm(self, udoc, dudoc, perm, ddoc=None):
     if not udoc or not dudoc:
       return False
     # TODO(iceboy): Fix caller when dudoc=None is passed in.
     role = dudoc.get('role', builtin.ROLE_DEFAULT)
-    mask = domain.get_all_roles(self.domain).get(role, builtin.PERM_NONE)
+    mask = domain.get_all_roles(ddoc if ddoc else self.domain).get(role, builtin.PERM_NONE)
     return ((perm & mask) == perm
             or self.udoc_has_priv(udoc, builtin.PRIV_MANAGE_ALL_DOMAIN))
 

vj4/locale/zh_CN.yaml
@@ -774,3 +774,19 @@ home_domain_account: 当前域的设置
 'The value `{1}` of {0} already exists.': '{0} 的值 `{1}` 已经存在。'
 Display name {1} you want to set is used by others.: 您想要设置的显示名 {1} 已经被其他人使用了。
 Numeric PID: 数字题号
+Only {0} problems can be copied in one request, got {1}.: 一次请求只能复制 {0} 个题目，但是您输入了 {1} 个。
+Problem is successfully copied.: 题目复制完成。
+problem_copy: 复制题目
+Copy Problem: 复制题目
+Be Copied: 被复制
+Copy from: 复制自
+Problem ID List: 题目 ID 列表
+Copy: 复制
+Destination Domain: 目标域
+Domain ID: 域 ID
+Test data comes from: 测试数据来自
+Copy Problem To: 将题目复制到
+What's this?: 这是什么？
+About test data: 关于测试数据
+With this feature, you can copy problems that you can view from a domain to some other domain. Their title, content, tags and categories will be copied. However, their test data are not copied directly.: 您可以通过这个功能将某域下您能查看的题目复制到其它域中，题目的标题、描述、标签与分类将被复制过来。但是测试数据不会被直接复制。
+Instead of copying the test data directly, the test data of the copied problems will be linked to the test data of the source problems (called the source test data). Thus, the copied problems can observe the changes in the source test data. The permissions of the test data of the copied problems follow the source test data, e.g., you still might not download them but the judges can. By uploading some new test data, the link will be broken and the new test data will be used.: 虽然测试数据不会被复制，但是系统会将题目的测试数据链接到原题。因此在原题的数据被改动的时候，复制后的题目的数据也会同时改动。链接后的测试数据的权限以原题为准，比如您可能依旧无法下载数据，但是评测机可以。在题目设置页面中可以通过上传新的测试数据的方式，这个数据链接会被删除，以后将使用您的新测试数据。

vj4/ui/misc/icons/copy.svg
@@ -1,0 +1,1 @@
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1000 1000"><path d="M623 254V8H192L8 192v554h369v246h615V254H623zM192 95v97H95l97-97zM69 685V254h185V69h308v185L377 438v247H69zm493-344v97h-98l98-97zm369 590H439V500h184V315h308v616z"/></svg>

commit d4edb1cd8988986dafa70132abb7aab140daebd9

Fix some bugs introduced in #495 and #499 (#502)

vj4/model/adaptor/problem.py
@@ -48,14 +48,13 @@ async def add(domain_id: str, title: str, content: str, owner_uid: int,
 
 async def copy(pdoc, dest_domain_id: str, owner_uid: int,
                pid: document.convert_doc_id=None, hidden: bool=False):
-  # This copies contents only, data will be referenced to the source problem.
+  # This copies contents only, and the data will be linked to the source problem.
   data = pdoc['data']
   src_domain_id, src_pid = pdoc['domain_id'], pdoc['doc_id']
-  if type(data) is objectid.ObjectId:
-    data = { 'domain': src_domain_id, 'pid': src_pid }
-  elif type(data) is dict:
+  if type(data) is dict:
     src_domain_id, src_pid = data['domain'], data['pid']
-
+  else:
+    data = {'domain': src_domain_id, 'pid': src_pid}
   pid = await add(domain_id=dest_domain_id, owner_uid=owner_uid,
                   title=pdoc['title'], content=pdoc['content'],
                   pid=pid, hidden=hidden, category=pdoc['category'],

vj4/model/domain.py
@@ -324,14 +324,18 @@ def get_join_settings(ddoc, now):
 
 @argmethod.wrap
 async def get_prefix_search(prefix: str, fields={}, limit: int=50):
+  builtin_ddocs = []
+  for domain in builtin.DOMAINS:
+    if domain['_id'][:len(prefix)] == prefix or domain['name'][:len(prefix)] == prefix:
+      builtin_ddocs.append(domain)
   regex = r'\A\Q{0}\E'.format(prefix.replace(r'\E', r'\E\\E\Q'))
   coll = db.coll('domain')
-  udocs = await coll.find({'$or': [{'_id': {'$regex': regex}},
+  ddocs = await coll.find({'$or': [{'_id': {'$regex': regex}},
                                    {'name': {'$regex': regex}}]},
                           projection=fields) \
                     .limit(limit) \
                     .to_list()
-  return udocs
+  return builtin_ddocs + ddocs
 
 
 @argmethod.wrap

vj4/ui/templates/problem_detail.html
@@ -53,8 +53,8 @@
           </blockquote>
         {% endif %}
         {% if pdoc['data'] is mapping %}
-          <blockquote>
-            <p>{{ _('Test data comes from') }} <a href="{{ reverse_url('problem_detail', domain_id=pdoc['data']['domain'], pid=pdoc['data']['pid']) }}">{{ pdoc['data']['domain'] }}/{{ pdoc['data']['pid'] }}</a></p>
+          <blockquote class="note">
+            <p>{{ _('Test data comes from') }} <a href="{{ reverse_url('problem_detail', domain_id=pdoc['data']['domain'], pid=pdoc['data']['pid']) }}" target="_blank">{{ pdoc['data']['domain'] }}/{{ pdoc['data']['pid'] }}</a></p>
           </blockquote>
         {% endif %}
         {% if tdoc %}

vj4/ui/templates/problem_upload.html
@@ -2,7 +2,7 @@
 {% block body %}
 <form method="post" enctype="multipart/form-data">
 {% if pdoc['data'] is mapping %}
-<p>{{ _('Test data comes from') }} <a href="{{ reverse_url('problem_detail', domain_id=pdoc['data']['domain'], pid=pdoc['data']['pid']) }}">{{ pdoc['data']['domain'] }}/{{ pdoc['data']['pid'] }}</a></p>
+<p>{{ _('Test data comes from') }} <a href="{{ reverse_url('problem_detail', domain_id=pdoc['data']['domain'], pid=pdoc['data']['pid']) }}" target="_blank">{{ pdoc['data']['domain'] }}/{{ pdoc['data']['pid'] }}</a></p>
 {% else %}
 <p>{{ _('Current dataset: {0}').format(pdoc['data']|default(_('(None)'))) }}</p>
 <p>{{ _('MD5: {0}').format(md5|default(_('(None)'))) }}</p>