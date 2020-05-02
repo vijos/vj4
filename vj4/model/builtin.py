6ba55b2aa5e0b568e0f770672313bbde6d25c053
MonKey Lee <zbaxrl@foxmail.com>
2020-05-02 22:21:44 +0800

    Update url for beian information (Fix #556) (#557)

--- vj4/model/builtin.py
+++ vj4/model/builtin.py
@@ -329,7 +329,7 @@ LEVELS = collections.OrderedDict([(10, 1),
 
 # Footer extra HTMLs. TODO(iceboy): remove.
 FOOTER_EXTRA_HTMLS = ['© 2005 - 2020 <a href="https://vijos.org/">Vijos.org</a>', version.get(),
-                      '<a href="http://www.miitbeian.gov.cn/" target="_blank" rel="nofollow">' +
+                      '<a href="http://beian.miit.gov.cn/" target="_blank" rel="nofollow">' +
                       '沪ICP备14040537号</a>']
 
 PROBLEM_CATEGORIES = collections.OrderedDict([
