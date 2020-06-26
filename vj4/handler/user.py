a26f885522c9d3e9b3a415b4a4f80b2c147ef32f
Wende Tan <twd2@163.com>
2020-06-27 02:01:23 +0800

    grumble

--- vj4/handler/user.py
+++ vj4/handler/user.py
@@ -264,7 +264,8 @@ class RankHandler(base.Handler):
   @base.route_argument
   @base.sanitize
   async def get(self, *, page: int=1):
-    udocs, uucount, ucount = await pagination.paginate(domain.get_multi_user(domain_id=self.domain_id, rp={'$gt': 0.0}).sort([('rank', 1)]),
-                                                       page, self.USERS_PER_PAGE)
-    udict = await user.get_dict(udoc['uid'] for udoc in udocs)
-    self.render('domain_ranking.html', page=page, uucount=uucount, ucount=ucount, udocs=udocs, udict=udict)
+    dudocs, dupcount, _ = await pagination.paginate(
+        domain.get_multi_user(domain_id=self.domain_id, rp={'$gt': 0.0}).sort([('rank', 1)]),
+        page, self.USERS_PER_PAGE)
+    udict = await user.get_dict(dudoc['uid'] for dudoc in dudocs)
+    self.render('domain_ranking.html', page=page, dupcount=dupcount, dudocs=dudocs, udict=udict)
