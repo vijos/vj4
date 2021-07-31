b8f4e9152e06f129f2ed847a69e38c63b3d2777b
Soha Jin <soha@lohu.info>
2021-07-31 10:33:41 +0800

    add GitHub Action workflow and remove Travis CI

--- scripts/build/main.js
+++ scripts/build/main.js
@@ -3,10 +3,11 @@ import root from './utils/root.js';
 import runGulp from './runGulp.js';
 import runWebpack from './runWebpack.js';
 
-const { watch, production } = argv;
+const { watch, production, constant } = argv;
 
 async function main() {
   await runGulp(argv);
+  if (constant) return;
   runWebpack(argv);
 }
 
